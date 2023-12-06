1093b6b5a71f0ba89545aa71ac23f8d0582703c8
Khalid <khcharkaoui@gmail.com>
2023-12-06 03:38:37 +0100

    gunicorn

--- app.py
+++ app.py
@@ -77,4 +77,4 @@ def delete_task(task_id):
     return jsonify({"tasks": [], "total_tasks": total_tasks, "pending_tasks": pending_tasks})
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(debug=False, host="0.0.0.0", port=int(os.environ.get("port", 5000)))
